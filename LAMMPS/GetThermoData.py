f00125c2765e9d11c68b8203d4eadae77fd4983f
mchchiang <michael_chiang_ch@hotmail.com>
2016-10-18 14:24:22 +0100

    Added python script to pull thermo data from lammps file

---
+++ LAMMPS/GetThermoData.py
@@ -0,0 +1,29 @@
+import sys
+
+args = sys.argv
+args.pop(0) #ignore self
+
+log_file = args.pop(0)
+output_file = args.pop(0)
+
+
+reader = open(log_file, "r")
+writer = open(output_file, "w")
+
+foundLine = False
+count = 0
+for line in reader:
+    if (line.startswith("Step")):
+        foundLine = True
+    if (foundLine):
+        count += 1
+    if (count == 2):
+        writer.write(line)
+        count = 0
+        foundLine = False
+
+reader.close()
+writer.close()
+    
+        
+    
