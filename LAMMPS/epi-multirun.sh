e3e50196a1bb03f704178c55f9cb1ec0989c2766
Michael Chiang <michael_chiang_ch@hotmail.com>
2016-11-01 22:33:36 +0000

    Wrote working script for running multiple trials

--- LAMMPS/epi-multirun.sh
+++ LAMMPS/epi-multirun.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-L=100
+L=150
 N=1000
 f_start=$1
 f_end=$2
@@ -9,42 +9,40 @@ e_start=$4
 e_end=$5
 e_inc=$6
 rc=2.5
-max_job=$7
-max_iter=$8
+max_run=$7
+run_shift=$8
+max_job=$9
+max_iter=5000
 teq=1000000
 dump=dump
 exepath="../Java/build/classes/"
-outdir=$9
+outdir=${10}
 nproc=1
 
 cmd=()
 log=()
-count=1
+jobid=1
 
 f=$f_start
-e=$e_start
 
 while (( $(bc <<< "$f <= $f_end") ))
 do
+    e=$e_start
     while (( $(bc <<< "$e <= $e_end") ))
     do
-	for (( run=1; run<$max_run; run++ ))
+	e=$(printf "%.1f" $e)
+	f=$(printf "%.1f" $f)
+	for (( run=1; run <= $max_run; run++ ))
 	do
-#	    cmd[count]="bash epigenetics.sh ${L} ${N} ${f} ${e} ${rc} ${max_iter} ${teq} ${run} ${dump} ${exepath} ${nproc} ${outdir}"
-	    log[count]="nohup_L_${L}_N_${N}_f_${f}_e_${e}_rc_${rc}_t_${max_iter}_run_${run}.log"
-	#    count=$(bc <<< "$count + 1")
-	    
-	    if (( $(bc <<< "$count > $max_job") )); then
-		for (( job=1; job<$max_job; job++ ))
-		do
-		    nohup ${cmd[job]} &> ${log[job]} &
-		done
-		wait
-		count=1
-		cmd=()
-		log=()
-	    fi
-
+	#    if [ run = 1 ]; then
+	#	dump=dump
+	#    else
+	#	dump=nodump
+	#    fi
+	    runid=$(bc <<< "$run + $run_shift")
+	    cmd[$jobid]="bash epigenetics.sh ${L} ${N} ${f} ${e} ${rc} ${max_iter} ${teq} ${runid} ${dump} ${exepath} ${nproc} ${outdir}"
+	    log[$jobid]="nohup_L_${L}_N_${N}_f_${f}_e_${e}_rc_${rc}_t_${max_iter}_run_${runid}.log"
+	    jobid=$(bc <<< "$jobid + 1")
 	done
 	
 	e=$(bc <<< "$e + $e_inc")
@@ -53,10 +51,18 @@ do
     f=$(bc <<< "$f + $f_inc")
 done
 
-if (( $(bc <<< "$count > 1 && $count <= $max_job") )); then
-    for (( job=1; job<$count; job++ ))
+total_jobs=$(bc <<< "$jobid - 1")
+jobid=1
+
+while (( $(bc <<< "$jobid <= $total_jobs") ))
+do
+    for ((i=1; i <= $max_job && $jobid <= $total_jobs ; i++))
     do
-	echo nohup ${cmd[job]} &> ${log[job]} &
+	echo "nohup ${cmd[jobid]} &> ${log[jobid]} &"
+	nohup ${cmd[jobid]} &> ${log[jobid]} &
+	#bash loop.sh $i &
+	jobid=$(bc <<< "$jobid + 1")
     done
     wait
-fi
+done
+
